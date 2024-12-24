0ad845f40f55ded63ae25ad93d64a59cf8201c1c
Layla-prog <laylamarhoon80@gmail.com>
2024-12-24 11:10:30 +0300

    Update Program.cs

--- Happy Journey Airline/Backend/Program.cs
+++ Happy Journey Airline/Backend/Program.cs
@@ -22,7 +22,7 @@ namespace Happy_Journey_Airline
 
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new  AdminMessage());
+                Application.Run(new  WelcomeScreen());
             }
         }
     }
