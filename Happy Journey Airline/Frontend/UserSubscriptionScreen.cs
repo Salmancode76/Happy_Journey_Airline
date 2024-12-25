efd75e51b60ad2ceebd6a2a10d7424d6f95bd857
Layla-prog <laylamarhoon80@gmail.com>
2024-12-25 16:40:30 +0300

    Update UserSubscriptionScreen.cs

--- Happy Journey Airline/Frontend/UserSubscriptionScreen.cs
+++ Happy Journey Airline/Frontend/UserSubscriptionScreen.cs
@@ -61,7 +61,6 @@ namespace Happy_Journey_Airline
         private void UserSubscriptionScreen_Load(object sender, EventArgs e)
         {
             LoadGrid();
-            //AdminSubscribersSubscription admin = new AdminSubscribersSubscription();
         }
 
         private void subscribeBttn_Click(object sender, EventArgs e)
@@ -93,7 +92,7 @@ namespace Happy_Journey_Airline
                 return Convert.ToInt32(subscriptionGrid.CurrentRow.Cells["subscription_id"].Value);
             }
         
-            // If no row is selected, return -1 or throw an exception
+            // If no row is selected, throw an exception
             throw new InvalidOperationException("No subscription is selected.");
         }
 
